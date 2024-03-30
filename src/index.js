f03cea85bfb81a06a121b0b7c113196f27684a93
EdQuinx <bjnboo833626@gmail.com>
2024-03-30 16:16:40 +0700

    add bootstrap and scss

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 const express = require('express')
+const path = require('path')
+const exphbs = require('express-handlebars')
 const morgan = require('morgan')
 const app = express()
 const port = 3000
 
+app.use(express.static(path.join(__dirname, 'public')))
+
+// HTTP Logger
 app.use(morgan('combined'))
 
+// Template engine
+app.engine('.hbs', exphbs.engine({
+  extname: '.hbs'
+}))
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, 'resources/views'))
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('home')
 })
 
 app.listen(port, () => {
